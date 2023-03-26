84ffbb9d8d7ca43468b601afef547ff7744d761b
Olavo Conceicao <orc@cin.ufpe.br>
2023-03-26 02:12:35 -0300

    <page> home-article: conexão com o backend

--- src/app/pages/home-article/home-article.component.ts
+++ src/app/pages/home-article/home-article.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { PostsService } from 'src/app/services/complaints/post.service';
+import { Post } from 'src/app/models/post.model';
 
 @Component({
   selector: 'app-home-article',
@@ -8,12 +10,15 @@ import { Component } from '@angular/core';
 export class HomeArticleComponent {
 
   constructor(
-
+    private postService: PostsService
   ) { }
+
+  listaPosts: any[] = [];
 
   ImagePrincipalArticle: boolean = false;
 
   ngOnInit(): void {
+    this.getPosts();
     this.showPrincipalArticle();
   }
 
@@ -21,6 +26,27 @@ export class HomeArticleComponent {
     setInterval(() => {
         this.ImagePrincipalArticle = true;
     },2000)
+  }
+
+    /*------------------------------------------------------*/
+   
+  //               Requisições para o backend
+
+  /*------------------------------------------------------*/
+
+  getPosts() {
+    this.postService.getPosts().subscribe((response: Post[]) => {
+      response.forEach(post => {
+        this.listaPosts.push(new Post(
+          post.userId,
+          post.id,
+          post.title,
+          post.body,
+        ));
+      })
+    }, error => {
+      console.log(error)
+    });
   }
 
 }
