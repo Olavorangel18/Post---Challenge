b6cffc1eb17e3cb4a99a9ee0f285088334af5ada
Olavo Conceicao <orc@cin.ufpe.br>
2023-03-26 01:15:32 -0300

    <page> home-article: controle de visualização de artigos

--- src/app/pages/home-article/home-article.component.ts
+++ src/app/pages/home-article/home-article.component.ts
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class HomeArticleComponent {
 
+  constructor(
+
+  ) { }
+
+  imageToShow: boolean = false;
+  ImagePrincipalArticle: boolean = false;
+
+  ngOnInit(): void {
+    this.showImages();
+    this.showPrincipalArticle();
+  }
+
+  showImages(){
+    setInterval(() => {
+        this.imageToShow = true;
+    },1000)
+  }
+
+  showPrincipalArticle(){
+    setInterval(() => {
+        this.ImagePrincipalArticle = true;
+    },2000)
+  }
+
 }
