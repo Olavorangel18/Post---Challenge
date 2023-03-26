565068c0c9a9a2e27c810865c2fafe8c22987969
Olavo Conceicao <orc@cin.ufpe.br>
2023-03-26 08:49:14 -0300

    <page> post: capturar id da postagem

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeArticleComponent } from './pages/home-article/home-article.component';
+import { PostPageComponent } from './pages/post-page/post-page.component';
 
 const routes: Routes = [
   {path : '', component : HomeArticleComponent},
+  {path : '/post/:id', component : PostPageComponent},
 ];
 
 @NgModule({
--- src/app/pages/post-page/post-page.component.ts
+++ src/app/pages/post-page/post-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: 'app-post-page',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./post-page.component.scss']
 })
 export class PostPageComponent {
+  constructor(
+    private route: ActivatedRoute,
+    ) { }
+    id: string = '';
 
+    ngOnInit(): void {
+      this.id = this.route.snapshot.params['id'];
+      console.log(this.id)
+    }
+    
 }
